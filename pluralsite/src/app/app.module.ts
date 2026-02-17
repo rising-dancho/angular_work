94d184d56296be5f183db1730fae8d858990a99c
rising-dancho <zenlaws02@gmail.com>
2026-02-18 00:55:45 +0800

    feat: currency pipe

--- pluralsite/src/app/app.module.ts
+++ pluralsite/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { SiteHeaderComponent } from './site-header/site-header.component';
     SiteHeaderComponent,
   ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'en-PH' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- pluralsite/src/main.ts
+++ pluralsite/src/main.ts
@@ -4,9 +4,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// philippine peso
+import localePh from '@angular/common/locales/en-PH';
+import { registerLocaleData } from '@angular/common';
+
+// ✅ register locale BEFORE bootstrapping
+registerLocaleData(localePh);
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+// PHILIPPINE LOCAL CURRENCY HOW TO:
+//  https://chatgpt.com/share/69949d32-5678-8000-9066-672e78afbf1a
