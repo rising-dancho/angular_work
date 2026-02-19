dcb4bdfff702e137f48d53607a73e014c418eb38
rising-dancho <zenlaws02@gmail.com>
2026-02-19 22:34:12 +0800

    feat: POST request http

--- pluralsite/src/app/cart.service.ts
+++ pluralsite/src/app/cart.service.ts
@@ -9,10 +9,14 @@ import { IProduct } from './catalog/product.model';
 export class CartService {
   private cart: IProduct[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   add(product: IProduct) {
     this.cart.push(product);
-    console.log(`product ${product.name} added to cart`);
+
+    // this javascript object this.cart is auto converted to JSON
+    this.http.post('/api/cart', this.cart).subscribe(() => {
+      console.log(`product ${product.name} added to cart`);
+    });
   }
 }
