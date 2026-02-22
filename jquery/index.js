68fee59b3861851609b9dcb97dfeea8a6017d16a
Jose A. Perez Jr <EI0009E@EMPLEYADO.COM>
2026-02-22 20:58:19 +0800

    feat: jquery

---
+++ jquery/index.js
@@ -0,0 +1,17 @@
+// $("h1").css("color", "red");
+// .css method
+// current value of the css property by simply having the name of the property
+console.log($("h1").css("font-size"));
+
+// add class to an html tag : can add multiple just add space in between
+$("h1").addClass("big-title padding-50");
+
+// removing class
+// $("h1").removeClass("big-title");
+
+$("button").css("background-color", "red");
+
+// check if an html tag has an element
+console.log($("h1").hasClass("padding-50"));
+
+// change text
