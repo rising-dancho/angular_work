f8c62259e06dbdea26788e4c979fcbce53379306
Jose A. Perez Jr <EI0009E@EMPLEYADO.COM>
2026-02-25 00:50:07 +0800

    feat: working with @Output()

---
+++ traversy/src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from 'src/app/mock-task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task!: Task;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
