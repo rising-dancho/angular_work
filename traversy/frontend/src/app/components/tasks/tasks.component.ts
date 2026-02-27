32def1dcfda435c74a381f318ca00e10eb0b10f0
Jose A. Perez Jr <EI0009E@EMPLEYADO.COM>
2026-02-27 10:18:34 +0800

    feat: deletion

--- traversy/frontend/src/app/components/tasks/tasks.component.ts
+++ traversy/frontend/src/app/components/tasks/tasks.component.ts
@@ -21,7 +21,7 @@ export class TasksComponent implements OnInit {
   }
 
   deleteTask(task: Task) {
-    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(task => task.id !== task.id)));
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)));
   }
 
 }
