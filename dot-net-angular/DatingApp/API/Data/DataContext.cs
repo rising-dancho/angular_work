c929b4f0394e60ad6b309691b5cc5c32a8994be7
rising-dancho <zenlaws02@gmail.com>
2026-05-26 03:57:01 +0800

    feat: updated folder structure

--- dot-net-angular/DatingApp/API/Data/DataContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-namespace API.Data;
-
-
-// IN ENTITY FRAMEWORK: AN ENTITY REPRESENTS A TABLE
-public class DataContext : Db
-{
-
-}
--- dotnet_angular/DatingApp/API/Controllers/MembersController.cs
+++ dotnet_angular/DatingApp/API/Controllers/MembersController.cs
@@ -1,6 +1,5 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
