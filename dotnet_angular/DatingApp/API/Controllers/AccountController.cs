d0cf8db85ba2ded23c11f5bc1f333a4a21122a75
Jose A. Perez Jr <ei009e@empleyado.com>
2026-06-02 20:24:37 +0800

    feat: controller

--- dotnet_angular/DatingApp/API/Controllers/AccountController.cs
+++ dotnet_angular/DatingApp/API/Controllers/AccountController.cs
@@ -1,28 +1,38 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context) : BaseApiController
 {
     [HttpPost("register")] //https://localhost:5001/api/account/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    // public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
         using var hmac = new HMACSHA512();
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
         // Adding a user in the Users table
         context.Users.Add(user);
         await context.SaveChangesAsync();
-
         return user;
     }
+
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+    }
+
+
 }
