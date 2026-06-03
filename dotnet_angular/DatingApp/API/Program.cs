73e50c9a90e2c896b37e09e54cfe378d6f7269d6
Jose A. Perez Jr <ei009e@empleyado.com>
2026-06-03 15:47:04 +0800

    feat: token key

--- dotnet_angular/DatingApp/API/Program.cs
+++ dotnet_angular/DatingApp/API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
--- dotnet_angular/DatingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ dotnet_angular/DatingApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0cf8db85ba2ded23c11f5bc1f333a4a21122a75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89da1897edcdfa74a0ebf37bf55dfc5ade55d9dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
